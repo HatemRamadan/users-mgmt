7c4ff84348400ce4932bcbb8948a96f0c574ce39
Ahmed Anwar <aanwar@sumerge.com>
2019-07-18 10:55:03 +0200

    Add post method with privileges for admin role only

--- src/main/java/com/sumerge/program/rest/UserResource.java
+++ src/main/java/com/sumerge/program/rest/UserResource.java
@@ -1,9 +1,13 @@
 package com.sumerge.program.rest;
 
+import com.sumerge.program.user.entity.User;
 import com.sumerge.program.user.entity.UserRepository;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -11,7 +15,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-import static javax.ws.rs.core.Response.Status.FORBIDDEN;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
 /**
  * @author Ahmed Anwar
@@ -20,6 +24,8 @@ import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 @RequestScoped
 public class UserResource
 {
+    private static final Logger LOGGER = Logger.getLogger(UserResource.class.getName());
+
     @Context
     private SecurityContext securityContext;
 
@@ -29,8 +35,30 @@ public class UserResource
     @GET
     @Produces(APPLICATION_JSON)
     public Response get() {
-        return securityContext.isUserInRole("admin")
-                ? Response.ok().entity(repo.getAllUsers()).build()
-                : Response.status(FORBIDDEN).build();
+		LOGGER.info("Entering get with user " + securityContext.getUserPrincipal().toString());
+		try {
+			return Response.ok().
+					entity(repo.getAllUsers()).
+					build();
+		} catch (Exception e) {
+			return Response.serverError().
+					entity(e).
+					build();
+		}
+    }
+
+    @POST
+    @Consumes(APPLICATION_JSON)
+    public Response post(User user) {
+        LOGGER.info("Entering post with user " + securityContext.getUserPrincipal().toString());
+		try {
+			return Response.ok().
+					entity(repo.addUser(user)).
+					build();
+		} catch (Exception e) {
+			return Response.serverError().
+					entity(e).
+					build();
+		}
     }
 }
--- src/main/java/com/sumerge/program/user/entity/UserRepository.java
+++ src/main/java/com/sumerge/program/user/entity/UserRepository.java
@@ -1,19 +1,40 @@
 package com.sumerge.program.user.entity;
 
 import java.util.List;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import static java.util.logging.Level.SEVERE;
 
 /**
  * @author Ahmed Anwar
  */
 @Stateless
 public class UserRepository {
+
+	private static final Logger LOGGER = Logger.getLogger(UserRepository.class.getName());
+
 	@PersistenceContext
 	private EntityManager em;
 
 	public List<User> getAllUsers() {
-		return em.createNamedQuery("User.findAll", User.class).getResultList();
+		try {
+			return em.createNamedQuery("User.findAll", User.class).getResultList();
+		} catch (Exception e) {
+			LOGGER.log(SEVERE, e.getMessage(), e);
+			throw e;
+		}
+	}
+
+	public boolean addUser(User user) {
+		try {
+			em.persist(user);
+			return true;
+		} catch (Exception e) {
+			LOGGER.log(SEVERE, e.getMessage(), e);
+			throw e;
+		}
 	}
 }
